e5ec0920117cf5bd32c2e0ab55f22b204bd0744d
im977 <inkoghuto@gmail.com>
2023-05-01 16:04:31 +0300

    add null check in number_of_sim

--- src/phone.py
+++ src/phone.py
@@ -4,6 +4,8 @@ from src.item import Item
 class Phone(Item):
     def __init__(self, name: str, price: float, quantity: int, number_of_sim: int) -> None:
         super().__init__(name, price, quantity)
+        if number_of_sim == 0:
+            raise ValueError('У телефона не может быть 0 сим-карт')
         self.number_of_sim = number_of_sim
 
     def __repr__(self):
